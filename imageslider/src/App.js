2c225eb19416f12dd76eff37a7f00fff67aed8f2
unknown <fidhalatheefm@gmail.com>
2023-11-21 16:42:06 +0530

    completed image slider with parallax effect

--- imageslider/src/App.js
+++ imageslider/src/App.js
@@ -10,33 +10,29 @@ function App() {
   return (
     <div className="App">
       <Parallax strength={300} bgImage={Woman}>
-
         <div className="content">
-          <button className=" text-content ripple ripple-surface btn btn-primary btn-danger" role="button" >Normal Parallax</button>
+          <button className="text-content ripple ripple-surface btn btn-primary btn-danger">Normal Parallax</button>
           <div className="icon-content">
             <p className="small text-secondary">Scroll Down</p>
-            <i >
+            <i>
               <FontAwesomeIcon icon={faAngleDown} className="fa-beat" style={{ color: "white" }} />
             </i>
           </div>
         </div>
       </Parallax>
 
-
       <Parallax strength={300} blur={{ min: -5, max: 15 }} bgImage={City}>
         <div className="content">
-
           <div className="icon-contentup">
-            <p className="small text-secondary">Scroll Up</p>
-            <i >
+            <i>
               <FontAwesomeIcon icon={faAngleUp} className="fa-beat" style={{ color: "white" }} />
             </i>
+            <p className="small text-secondary">Scroll Up</p>
           </div>
-          <button className=" text-content ripple ripple-surface btn btn-primary btn-secondary" role="button" >Blur Parallax</button>
-
+          <button className="text-content ripple ripple-surface btn btn-primary btn-secondary">Blur Parallax</button>
           <div className="icon-content">
             <p className="small text-secondary">Scroll Down</p>
-            <i >
+            <i>
               <FontAwesomeIcon icon={faAngleDown} className="fa-beat" style={{ color: "white" }} />
             </i>
           </div>
@@ -46,23 +42,21 @@ function App() {
       <Parallax strength={-600} bgImage={Fly}>
         <div className="content">
           <div className="icon-contentup">
-            <p className="small text-secondary">Scroll Up</p>
-            <i >
+            <i>
               <FontAwesomeIcon icon={faAngleUp} className="fa-beat" style={{ color: "white" }} />
             </i>
+            <p className="small text-secondary">Scroll Up</p>
           </div>
-          <button className=" text-content ripple ripple-surface btn btn-primary btn-primary" role="button" >Reverse Parallax</button>
-
+          <button className="text-content ripple ripple-surface btn btn-primary btn-primary">Reverse Parallax</button>
           <div className="icon-content">
             <p className="small text-secondary">Scroll Down</p>
-            <i >
+            <i>
               <FontAwesomeIcon icon={faAngleDown} className="fa-beat" style={{ color: "white" }} />
             </i>
           </div>
         </div>
       </Parallax>
 
-      <div className="content"></div>
     </div>
   );
 }
