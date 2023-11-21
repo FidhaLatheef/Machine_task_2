96ba01fa16609fb478eac94acecf4e1c8ba0fcef
unknown <fidhalatheefm@gmail.com>
2023-11-21 15:34:17 +0530

    created a parallax effect slider and applied some styles

--- imageslider/src/App.js
+++ imageslider/src/App.js
@@ -1,28 +1,41 @@
-import React from 'react';
-import ImageSlider from './ImageSlider'; // Assuming the component is in the same directory
+import "./css/App.css";
+import { Parallax } from "react-parallax";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
+import Woman from "./images/c1.jpg";
+import City from "./images/c2.jpg";
+import Fly from "./images/c3.jpg";
 
-const App = () => {
-  // Define your images array
-  const images = [
-    'https://example.com/image1.jpg',
-    'https://example.com/image2.jpg',
-    'https://example.com/image3.jpg',
-  ];
-
-  // Set custom values for parallaxIntensity and transitionSpeed
-  const parallaxIntensity = 15;
-  const transitionSpeed = 700;
-
+function App() {
   return (
-    <div>
-      <h1>Custom Image Slider</h1>
-      <ImageSlider
-        images={images}
-        parallaxIntensity={parallaxIntensity}
-        transitionSpeed={transitionSpeed}
-      />
+    <div className="App">
+       <Parallax strength={300} bgImage={Woman}>
+        <div className="content">
+          <div className="text-content">Normal Parallax</div>
+          <div className="icon-content">
+            <i >
+              <FontAwesomeIcon icon={faAngleDown} className="fa-beat" style={{ color: "white" }} />
+            </i>
+          </div>
+        </div>
+      </Parallax>
+      
+
+      <Parallax strength={300} blur={{ min: -5, max: 15 }} bgImage={City}>
+        <div className="content">
+          <div className="text-content">Blur</div>
+        </div>
+      </Parallax>
+
+      <Parallax strength={-600} bgImage={Fly}>
+        <div className="content">
+          <div className="text-content">Reverse Parallax</div>
+        </div>
+      </Parallax>
+
+      <div className="content"></div>
     </div>
   );
-};
+}
 
 export default App;
